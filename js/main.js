1dfceeda53e6c010854dc8d36723d9e7eba87c41
Savitski-k <Savitski.k.v@gmail.com>
2020-03-16 03:00:34 +0300

    swiper projects

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 $(document).ready(function () {
   var modal = $('.modal'),
-      modalBtn = $('[data-toggle="modal"]'),
-      closeBtn = $('.modal__close');
+    modalBtn = $('[data-toggle="modal"]'),
+    closeBtn = $('.modal__close');
 
   modalBtn.on('click', function () {
     modal.toggleClass('modal--visible');
@@ -27,4 +27,25 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10)
+  bullets.css('left', prev.width() + 10)
+
+
 });
