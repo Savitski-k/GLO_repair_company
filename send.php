b5ce52c3cfeea63af5f8b6d012d07b94847e94a9
Savitski-k <Savitski.k.v@gmail.com>
2020-03-27 15:00:32 +0300

    homework

--- send.php
+++ send.php
@@ -4,32 +4,29 @@ $userName = $_POST['userName'];
 $userEmail = $_POST['userEmail'];
 $userPhone = $_POST['userPhone'];
 
-// Load Composer's autoloader
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
-// Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 
 try {
-    //Server settings
-    $mail->SMTPDebug = 0;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.yandex.ru';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 's1mpleOthings@yandex.ru';                     // SMTP username
-    $mail->Password   = '.qx8_Z5Y-X&,&F_';                               // SMTP password
-    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    $mail->Port       = 465;                                    // TCP port to connect to
+    
+    $mail->SMTPDebug = 0;                      
+    $mail->isSMTP();                                           
+    $mail->Host       = 'smtp.yandex.ru';                    
+    $mail->SMTPAuth   = true;                                  
+    $mail->Username   = 'r.repairdesign@yandex.by';                    
+    $mail->Password   = '3846208qweasD';                              
+    $mail->SMTPSecure = 'ssl';         
+    $mail->Port       = 465;                                    
     $mail->CharSet    = "utf-8";
 
-    //Recipients
-    $mail->setFrom('s1mpleOthings@yandex.ru');
-    $mail->addAddress('gghf@mail.ru');     // Add a recipient
+    $mail->setFrom('r.repairdesign@yandex.by');
+    $mail->addAddress('Savitski.k@mail.ru');     
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
+
+    $mail->isHTML(true);                                 
     $mail->Subject = 'Новая заявка с сайта';
     $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}";
 
